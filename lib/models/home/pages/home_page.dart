185b83402548b4b9442043466830c14b6e942b9c
Brian Schultz <brianschultz320@gmail.com>
2023-06-05 14:19:59 -0300

    create mocktail

--- lib/models/home/pages/home_page.dart
+++ lib/models/home/pages/home_page.dart
@@ -5,6 +5,7 @@ import 'home_page_state.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  // Sobrescrita do método createState() para criar uma instância de HomePageState
   @override
   HomePageState createState() => HomePageState();
 }
